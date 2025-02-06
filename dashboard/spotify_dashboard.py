4c7ca26b05ae6eb7153007e73cf34fba89cad867
Michael Nejaime <mnejaime@icloud.com>
2025-02-06 12:46:18 -0500

    refactor: remove unused plotly imports and clean up requirements

--- dashboard/spotify_dashboard.py
+++ dashboard/spotify_dashboard.py
@@ -3,8 +3,6 @@ import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
-import plotly.express as px
-import plotly.graph_objects as go
 
 load_dotenv('.env') # load our .env file
 
